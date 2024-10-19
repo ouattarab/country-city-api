a68ab58377337aa1a7cd1edd699d5b4346cc4578
Bakary Ouattara <ouattarab2011@gmail.com>
2024-10-19 19:40:33 +0200

    this is my first commit

---
+++ src/main/java/com/codercommune/CountryCityApiApplication.java
@@ -0,0 +1,13 @@
+package com.codercommune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryCityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryCityApiApplication.class, args);
+	}
+
+}
